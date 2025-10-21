aea362d028336cac6a82ad28577480f456f1415b
annie444 <6550634+annie444@users.noreply.github.com>
2025-10-21 21:07:20 +0000

    [2025-10-21 21:07:20] (Generate Skim completions) Updating completions

--- sk.fish
+++ sk.fish
@@ -1,3 +1,4 @@
+complete -c sk -l min-query-length -d 'Minimum query length to start showing results' -r
 complete -c sk -s t -l tiebreak -d 'Comma-separated list of sort criteria to apply when the scores are tied' -r -f -a "score\t''
 -score\t''
 begin\t''
@@ -58,6 +59,7 @@ zsh\t''"
 complete -c sk -l tmux -d 'Run in a tmux popup' -r
 complete -c sk -l hscroll-off -d 'Reserved for later use' -r
 complete -c sk -l jump-labels -d 'Reserved for later use' -r
+complete -c sk -l border -d 'Reserved for later use' -r
 complete -c sk -l tac -d 'Show results in reverse order'
 complete -c sk -l no-sort -d 'Do not sort the results'
 complete -c sk -s e -l exact -d 'Run in exact mode'
@@ -89,7 +91,6 @@ complete -c sk -s x -l extended -d 'Reserved for later use'
 complete -c sk -l literal -d 'Reserved for later use'
 complete -c sk -l cycle -d 'Reserved for later use'
 complete -c sk -l filepath-word -d 'Reserved for later use'
-complete -c sk -l border -d 'Reserved for later use'
 complete -c sk -l no-bold -d 'Reserved for later use'
 complete -c sk -l pointer -d 'Reserved for later use'
 complete -c sk -l marker -d 'Reserved for later use'
